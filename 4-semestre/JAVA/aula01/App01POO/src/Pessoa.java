99e686a92ac1ecb984cb6e63f9e3970c2b23f508
niltinfilho <niltonfilho115@gmail.com>
2026-08-07 21:50:01 -0300

    criando projeto e classe pessoa

---
+++ 4-semestre/JAVA/aula01/App01POO/src/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    int idade;
+    String nome;
+    float altura;
+
+    void correr() {
+        System.out.println("Correndo...");
+    }
+
+    void dormir() {
+        System.out.println("Dormindo...");
+    }
+}
