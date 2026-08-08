c34deb1e94f49916f6e8bddc1a51fae39ccbacb0
niltinfilho <niltonfilho115@gmail.com>
2026-08-08 16:12:31 -0300

    criando controller para criação de usuário

---
+++ 4-semestre/REACT-NODE/pizzaria/backend/src/controllers/user/CreateUserController.ts
@@ -0,0 +1,31 @@
+// Importa os tipos Request e Response do Express (para tipagem)
+import { Request, Response } from "express";
+
+// Importa o serviço responsável pela lógica de criação de usuário
+import { CreateUserService } from "../../services/user/CreateUserService";
+
+// Define a classe do controller (responsável por lidar com a requisição HTTP)
+class CreateUserController {
+
+  // Método que será chamdo quando a rota for acessada
+  async handle(req: Request, res: Response) {
+
+    // Desestrutura os dados enviados no corpo da requisição (body)
+    const { name, email, password } = req.body;
+
+    // Exibe os dados recebidos no console (útil para debugar)
+    console.log({ name, email, password });
+
+    // Cria uma instância do service
+    const createUserService = new CreateUserService();
+
+    // Executa a lógica de criação de usuário e aguarda o retorno
+    const user = await createUserService.execute();
+
+    // Retorna a resposta em JSON para o cliente
+    res.json({ message: user });
+  }
+}
+
+// Exporta o controller para ser usado nas rotas
+export { CreateUserController };
