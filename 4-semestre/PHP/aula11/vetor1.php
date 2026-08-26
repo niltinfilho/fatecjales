d146abfd1e4c512d4ab041c025a06453ac9cdcc1
niltinfilho <niltonfilho115@gmail.com>
2026-08-26 19:24:22 -0300

    comecando exercicios da aula 11 de php

--- 4-semestre/PHP/aula11/vetor1.php
+++ 4-semestre/PHP/aula11/vetor1.php
@@ -10,8 +10,22 @@
 
 <body>
   <?php
-
+  $numeros = array(12, 38, 55, 90, 110);
+  echo "<h1>Mostrando com print_r</h1>";
+  print_r($numeros);
+  echo "<h1>Mostrando com var_dump</h1>";
+  var_dump($numeros);
   ?>
+
+  <pre>
+      <?php
+      $numeros = array(12, 38, 55, 90, 110);
+      echo "<h1>Mostrando com print_r</h1>";
+      print_r($numeros);
+      echo "<h1>Mostrando com var_dump</h1>";
+      var_dump($numeros);
+      ?>
+  </pre>
 </body>
 
 </html>
